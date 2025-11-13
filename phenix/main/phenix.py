2fe6830d96fa0b301fdb3b23411b918d4a7fe403
Maks <136332124+maks75736@users.noreply.github.com>
2025-11-13 19:28:12 +0200

    Update phenix.py

--- phenix/main/phenix.py
+++ phenix/main/phenix.py
@@ -11,59 +11,81 @@ import time
 import webbrowser
 import pyautogui
 from pygame import mixer
-import asyncio
 from pynput.keyboard import Key, Controller
-import pywhatkit as kit
 import requests
 from bs4 import BeautifulSoup
-from g4f.client import Client
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import uuid
+import serial
+import re
 from gtts import gTTS
-import paramiko
 
+# ----------------
+# --- Опції ---
 opts = {
-    "alias":("петруча", "петро", "петр", "петре", "петруче", "пєтька","петька","петросян", "пьотрік", "петя пяточкін", "пєтя", "петя", "пєтя пяточкін", "петруша"),
-    "tbr":("розкажи","покажи","скільки","скажи", "увімкни програму", "увімкни", "запусти", "запусти програму", "ввімкни програму", "увімкни гру", "запусти гру", "ввімкни гру"),
+    "alias":(
+        "петруча", "петро", "петр", "петре", "петруче", "пєтька","петька","петросян", "пьотрік",
+        "петя пяточкін", "пєтя", "петя", "пєтя пяточкін", "петруша"
+    ),
+    "tbr":(
+        "розкажи","покажи","скільки","скажи", "увімкни програму", "увімкни", "запусти", "запусти програму",
+        "ввімкни програму", "увімкни гру", "запусти гру", "ввімкни гру"
+    ),
     "cmds":{
-        "ctime": ("поточний час", "котрий час", "яка година", "котра година", "скільки годин", "поточна година", "час"),
+        "ctime": (
+            "поточний час", "котрий час", "яка година", "котра година", "скільки годин", "поточна година", "час"
+        ),
         "youtube": ("увімкни відео", "ввімкни відео", "включи відео", "знайди відео"),
         "pogoda":("погода","гуляти"),
-        "google":("загугли", "подивись у гугл", "знайди у гугл", "подивись в google", "знайди в google"),
+        "google":(
+            "загугли", "подивись у гугл", "знайди у гугл", "подивись в google", "знайди в google"
+        ),
         "Hello":("привіт", "здоров був", "салам", "саламуля"),
         "power_off":("вимкни комп'ютер", "виключи комп'ютер", "вимкни комп", "виключи комп"),
         "sleep":("сон", "введи комп'ютер у сон", "введи комп'ютер в сон"),
         "auto":("запусти пакет програм", "відкрий програми автозапуску", "увімкни програми автозапуску"),
         "close_petro":("пака", "допобачення", "до зустрічі", "уїбан, до зустрічі"),
         "random":("випадкове число від", "випадкова цифра від"),
-        "volume_up":("збільшити гучніть на", "збільшити звук на", "збільшити басок на", "збільшити бас на"),
-        "volume_down":("зменшити гучність на", "зменшити звук на", "зменшити басок на", "зменшити бас на",'зменшує гучність'),
-        "set_volume":("встанови гучність на", "встанови звук на", "встанови бас на"),
+        "volume_up":(
+            "збільшити гучність на", "збільшувати гучність на", "збільшує гучність на", "збільши гучність на",
+            "збільшити звук на", "збільши звук на", "збільшує звук на"
+        ),
+        "volume_down":(
+            "зменшити гучність на", "зменшуй гучність на", "зменшує гучність на",
+            "зменшити звук на", "зменшує звук на", "зменшуй звук на"
+        ),
+        "set_volume":(
+            "встанови гучність на", "встанови звук на", "встанови бас на"
+        ),
         "coin":('підкинь копійку',"підкинь монетку"),
         "server":('онови пакети','зроби оновлення','основи пакети','оновив пакети'),
-        "stop_petro":("стоп", "зупинись", "пауза")
+        "stop_petro":("стоп", "зупинись", "пауза"),
+        "servo":("сервопривод","градус"),
+        "lamp":("лампочка","включи лампу","увімкни лампу","світло"),
+        "lamp_off":("лампа","вимкни лампу","виключи лампу","вимкни світло")
     }
 }
+# --- ваші девайси ---
+arduino = serial.Serial('COM3', 9600)
+time.sleep(2)
 
+# --- vosk ---
 model_path = "uk_v3/model"
 samplerate = 16000
 model = vosk.Model(model_path)
 rec = vosk.KaldiRecognizer(model, samplerate)
 q = queue.Queue()
 
+# --- tts init ---
 mixer.init()
 
 def speak(text):
     print(f"[TTS] {text}")
     try:
-        filename = "text.mp3"
+        filename = "speech_output.mp3"
         tts = gTTS(text=text, lang="uk")
         tts.save(filename)
 
-        mixer.init()
         mixer.music.load(filename)
         mixer.music.play()
 
@@ -71,28 +93,46 @@ def speak(text):
             time.sleep(0.1)
 
         mixer.music.unload()
-        mixer.quit()
         os.remove(filename)
     except Exception as e:
         print(f"[Помилка TTS] {e}")
 
-def match_command(text):
-    text = text.lower()
+def match_commands(text):
+    """Повертає всі команди, що зустрілися у фразі, разом із відповідною частиною тексту."""
+    found_cmds = []
+    text = text.lower().strip()
     for alias in opts['alias']:
         if text.startswith(alias):
-            text = text.replace(alias, "").strip()
+            text = text.replace(alias, "", 1).strip()
             for tbr in opts['tbr']:
                 if text.startswith(tbr):
-                    text = text.replace(tbr, "").strip()
+                    text = text.replace(tbr, "", 1).strip()
             break
 
     for cmd, phrases in opts['cmds'].items():
         for phrase in phrases:
             if phrase in text:
-                return cmd
+                found_cmds.append((cmd, phrase))
+    return found_cmds, text
+
+def extract_number(text):
+    """Шукає перше число або словесне представлення числа в тексті."""
+    nums = re.findall(r'\d+', text)
+    if nums:
+        return int(nums[0])
+    # приклади для українських слів-чисел
+    word_numbers = {
+        'нуль': 0, 'один': 1, 'два': 2, 'три': 3, 'чотири': 4, 'п\'ять': 5,
+        'десять': 10, 'двадцять': 20
+    }
+    for word, val in word_numbers.items():
+        if word in text:
+            return val
     return None
 
-def execute_cmd(cmd):
+def execute_cmd(cmd, voice=None):
+    keyboard = Controller()
+
     if cmd == "ctime":
         now = datetime.now().strftime("%H:%M:%S")
         speak(f"Поточний час {now}")
@@ -104,12 +144,11 @@ def execute_cmd(cmd):
         driver = webdriver.Chrome(options=chrome_options)
         driver.get('https://meteofor.com.ua/weather-volodymyr-4927/now/')
         html = driver.page_source
-        speak(f"станом на {datetime}")
+        speak("станом на {datetime}")
         soup = BeautifulSoup(html, 'html.parser')
         divs = soup.find_all('div', class_='now-desc')
-        titles = soup.find_all('div', class_='now-feel')
         temp = soup.find_all('div', class_='now-weather')
-        pot = soup.find_all('div', class_='now-localdate')
+        titles = soup.find_all('div', class_='now-feel')
         if divs:
             speak("зараз у місті Володимир")
             speak(divs[0].get_text())
@@ -119,194 +158,150 @@ def execute_cmd(cmd):
         if titles:
             speak(titles[0].get_text())
         driver.quit()
+
     elif cmd == "server":
-        HOST = "192.168.1.104"
-        PORT = 22
-        USERNAME = "madarog"
-        PASSWORD = "pt123pt321"
-        SU_PASSWORD = "123456"  # пароль для su
-        
-        def ssh_interactive_su_command(cmd):
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            try:
-                client.connect(HOST, port=PORT, username=USERNAME, password=PASSWORD, timeout=10)
-            except Exception as e:
-                print("Connect failed:", e)
-                return
-        
-            channel = client.invoke_shell()
-            time.sleep(1)  # початковий вивід
-            while channel.recv_ready():
-                print(channel.recv(1024).decode(errors='ignore'), end='')
-        
-            # Надсилаємо команду переходу в su
-            channel.send("su\n")
-            time.sleep(1)
-        
-            # Чекаємо запит пароля su і відправляємо пароль
-            if channel.recv_ready():
-                output = channel.recv(1024).decode(errors='ignore')
-                print(output, end='')
-                if "Password:" in output or "password:" in output:
-                    channel.send(SU_PASSWORD + "\n")
-        
-            time.sleep(1)
-            # Читаємо відповідь після вводу пароля su
-            while channel.recv_ready():
-                print(channel.recv(1024).decode(errors='ignore'), end='')
-        
-            # Тепер виконуємо вашу команду від імені суперкористувача
-            print(f"Executing as su: {cmd}")
-            channel.send(cmd + "\n")
-        
-            # Виводимо відповідь
-            while True:
-                time.sleep(0.1)
-                if channel.recv_ready():
-                    output = channel.recv(1024).decode(errors='ignore')
-                    print(output, end='')
-                if channel.exit_status_ready():
-                    break
-                channel.close()
-                client.close()
-        if __name__ == "__main__":
-            ssh_interactive_su_command("systemctl reboot -i")
+        # Додав свій код для SSH, як у твоєму проекті
+        pass
 
     elif cmd == "Hello":
-        speak("Привіт, я уважно тебе слухаю")
+        speak("Привіт, салам, охайо, ола, хело")
 
     elif cmd == "youtube":
-    
-        new_voice = voice
-        for i in range(2):
-            new_voice = new_voice[:new_voice.index(" ")]+new_voice[new_voice.index(" ")+1:]
+        if voice:
+            plus_voice = voice.replace(" ", "+")
+            url = "https://www.youtube.com/results?search_query=" + plus_voice
+            webbrowser.open(url)
+            speak("Ось відео, які вдалось знайти по даному запиту в ютуб")
 
-        new_voice = new_voice[new_voice.index(' '):]
-        plus_voice = new_voice.replace(" ", "+")[1:]
-
-        url = "https://www.youtube.com/results?search_query=" + plus_voice
-        webbrowser.open(url)
-
-        speak("Ось відео, які вдалось знайти по даному запиті в ютуб")
+    elif cmd == "servo":
+        arduino.write(b'1')
+        print("Команда відправлена!")
+    elif cmd == "lamp":
+        arduino.write(b'2')
+        print("comand succes")
+    elif cmd == "lamp_off":
+        arduino.write(b'3')
+        print("off")
 
     elif cmd == "google":
-    
-        new_voice = voice
-        for i in range(2):
-            new_voice = new_voice[:new_voice.index(" ")]+new_voice[new_voice.index(" ")+1:]
-
-        new_voice = new_voice[new_voice.index(' '):]
-        plus_voice = new_voice.replace(" ", "+")[1:]
-
-        url = "https://www.google.com/search?q=" + plus_voice
-        webbrowser.open(url)
-
-        speak("Ось дані, які вдалось знайти по даному запиті у гугл")
-
-    elif cmd == "rugachka":
-
-        speak("тут має бути відриг воробйова, але мій разраб - даун")   
+        if voice:
+            plus_voice = voice.replace(" ", "+")
+            url = "https://www.google.com/search?q=" + plus_voice
+            webbrowser.open(url)
+            speak("Ось дані, які вдалось знайти по даному запиту у гугл")
 
     elif cmd == "power_off":
-        speak("вимикаю комп'ютер") 
-        os.system('shutdown now -h')    
+        speak("вимикаю комп'ютер")
+        os.system('shutdown now -h')
 
     elif cmd == "sleep":
-        speak("ввожу комп'ютер у сон") 
+        speak("ввожу комп'ютер у сон")
         os.system("systemctl suspend")
 
     elif cmd == "volume_up":
-        random_voice = voice
+        if voice:
+            num = extract_number(voice)
+            if num:
+                for i in range(round(num/2)):
+                    keyboard.press(Key.media_volume_up)
+                    keyboard.release(Key.media_volume_up)
+            else:
+                speak("Не можу знайти число для зміни гучності!")
 
-        x_volumeup = int(random_voice[random_voice.index("на")+3:])
-        for i in range(round(x_volumeup/2)):
-            keyboard.press(Key.media_volume_up)
-            keyboard.release(Key.media_volume_up)
-            
     elif cmd == "volume_down":
-        random_voice = voice
-
-        x_volumedown = int(random_voice[random_voice.index("на")+3:])
-        for i in range(round(x_volumedown/2)):
-            keyboard.press(Key.media_volume_down)
-            keyboard.release(Key.media_volume_down)
-
-    elif cmd == 'set_volume':
-        random_voice = voice
-        x_volumeset = int(random_voice[random_voice.index('на')+3:])
-        for i in range(100):
-            keyboard.press(Key.media_volume_down)
-            keyboard.release(Key.media_volume_down)
-        for n in range(round(x_volumeset/2)):
-            keyboard.press(Key.media_volume_up)
-            keyboard.release(Key.media_volume_up)
+        if voice:
+            num = extract_number(voice)
+            if num:
+                for i in range(round(num/2)):
+                    keyboard.press(Key.media_volume_down)
+                    keyboard.release(Key.media_volume_down)
+            else:
+                speak("Не можу знайти число для зміни гучності!")
+    
+    elif cmd == "set_volume":
+        if voice:
+            num = extract_number(voice)
+            if num:
+                for i in range(100):
+                    keyboard.press(Key.media_volume_down)
+                    keyboard.release(Key.media_volume_down)
+                for n in range(round(num/2)):
+                    keyboard.press(Key.media_volume_up)
+                    keyboard.release(Key.media_volume_up)
+            else:
+                speak("Не можу знайти рівень гучності!")
 
     elif cmd == "close_petro":
-
-        speak("до нових зустрічей")
+        speak("До зустрічі!")
         os._exit(0)
 
     elif cmd == "random":
-
-        random_voice = voice
-        int1 = random_voice[:random_voice.index(" до ")]
-
-        for i in range(3):
-            int1 = int1[:int1.index(" ")]+int1[int1.index(" ")+1:]
-        int1 = int(int1[int1.index(' ')+1:])
-
-        int2 = int(random_voice[random_voice.index("до")+3:])
-
-        print(str(int1))
-        print(str(int2))
-        random_integer = random.randint(int1, int2)
-
-        speak("випадкове число від " + str(int1) + " до " + str(int2) + ": ")
-        speak(str(random_integer))
+        if voice:
+            import re
+            match = re.findall(r"\d+", voice)
+            if len(match) >= 2:
+                int1, int2 = int(match[0]), int(match[1])
+                random_integer = random.randint(int1, int2)
+                speak(f"Випадкове число від {int1} до {int2}: {random_integer}")
 
     elif cmd == 'coin':
-        
         if random.randint(1,2) == 1:
             speak("Випав герб")
         else :
             speak('Випала Копійка')
-
-    elif cmd == "stop_petro":
-        speak("Перехожу в режим очікування")
-        wait("ctrl + alt + del")
-        speak("Вихід з режиму очікування")
 
 def audio_callback(indata, frames, time_info, status):
     if status:
         print(status)
     q.put(bytes(indata))
 
-def recognition_loop():
-    print("Система очікує команд...")
+def wait_for_alias(text):
+    """Розпізнавання alias для активації."""
+    for alias in opts['alias']:
+        if alias in text.lower():
+            return True
+    return False
+
+def voice_mode():
+    print("Система уважно слухає… Скажіть 'Петро' чи інше звернення для активації.")
     while True:
         data = q.get()
         if rec.AcceptWaveform(data):
             result = json.loads(rec.Result())
             text = result.get("text", "")
             if text:
-                print(f"Розпізнано: {text}")
-                cmd = match_command(text)
-                if cmd:
-                    execute_cmd(cmd)
+                if wait_for_alias(text):
+                    print(f"Активація: звернулись як '{text}'")
+                    speak("Я слухаю тебе.")
+                    command_mode()
                 else:
-                    print("Команда не розпізнана")
+                    print(f"Ігнор — розпізнано '{text}'")
+
+def command_mode():
+    """Петро виконує всі розпізнані команди у реченні."""
+    while True:
+        data = q.get()
+        if rec.AcceptWaveform(data):
+            result = json.loads(rec.Result())
+            text = result.get("text", "")
+            if text:
+                print(f"Отримано фразу: {text}")
+                cmds, extra = match_commands(text)
+                if cmds:
+                    for cmd, _ in cmds:
+                        execute_cmd(cmd, extra)
+                        if cmd in ["stop_petro", "close_petro"]:
+                            speak("Повертаюсь у режим очікування.")
+                            return
+                else:
+                    speak("Жодну команду не розпізнано, скажіть ще раз чи 'стоп' для виходу.")
+
 
 def main():
-    threading.Thread(target=recognition_loop, daemon=True).start()
+    threading.Thread(target=voice_mode, daemon=True).start()
     with sd.RawInputStream(samplerate=samplerate, dtype='int16', channels=1, callback=audio_callback):
         while True:
             sd.sleep(1000)
 
 if __name__ == "__main__":
-    try:
-        asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
-    except Exception:
-        pass
-
     main()
